ab2b2032ef4c40554f808350063e8f158cccab93
pites322 <Best.pites@gmail.com>
2018-09-24 17:23:29 +0300

    work in progress

--- projectplace/urls.py
+++ projectplace/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.Title, name='Title'),
+    url(r'^post/new/$', views.post_new, name='post_new'),
 ]
--- projectplace/views.py
+++ projectplace/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
+from django.shortcuts import redirect
 #
 def Title(request):
     return render(request, 'projectplace/Title.html', {})
-# Create your views here.
+
+def post_new(request):
+    def post_new(request):
+        if request.method == "POST":
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.published_date = timezone.now()
+                post.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = PostForm()
+        return render(request, 'projectplace/post_edit.html', {'form': form})
+
+    # if request.method == "POST":
+    #     form = PostForm(request.POST)
+    #     if form.is_valid():
+    #         post = form.save(commit=False)
+    #         post.author = request.user
+    #         post.published_date = timezone.now()
+    #         post.save()
+    #         return redirect('post_detail', pk=post.pk)
+    # else:
+    #     form = PostForm()
+    # return render(request, 'blog/post_edit.html', {'form': form})
